f8b95d839867b291c0a5dd9a242496fdf5ec2f37
Cristina <cristina.ionescu06@gmail.com>
2024-06-15 22:00:47 +0300

    Day1 exercise renaming & cleanup

---
+++ Lab/day1/Day1_ex5/Debug/component/button/fsl_component_button.d
@@ -0,0 +1,37 @@
+component/button/fsl_component_button.o \
+ component/button/fsl_component_button.d: \
+ ../component/button/fsl_component_button.c \
+ C:\Users\crist\Documents\work\summer-school\summerschool\Lab\day1\LPC55S69_ex5\component\gpio/fsl_adapter_gpio.h \
+ C:\Users\crist\Documents\work\summer-school\summerschool\Lab\day1\LPC55S69_ex5\drivers/fsl_common.h \
+ C:\Users\crist\Documents\work\summer-school\summerschool\Lab\day1\LPC55S69_ex5\device/fsl_device_registers.h \
+ C:\Users\crist\Documents\work\summer-school\summerschool\Lab\day1\LPC55S69_ex5\device/LPC55S69_cm33_core0.h \
+ C:\Users\crist\Documents\work\summer-school\summerschool\Lab\day1\LPC55S69_ex5\CMSIS/core_cm33.h \
+ C:\Users\crist\Documents\work\summer-school\summerschool\Lab\day1\LPC55S69_ex5\CMSIS/cmsis_version.h \
+ C:\Users\crist\Documents\work\summer-school\summerschool\Lab\day1\LPC55S69_ex5\CMSIS/cmsis_compiler.h \
+ C:\Users\crist\Documents\work\summer-school\summerschool\Lab\day1\LPC55S69_ex5\CMSIS/cmsis_gcc.h \
+ C:\Users\crist\Documents\work\summer-school\summerschool\Lab\day1\LPC55S69_ex5\CMSIS/mpu_armv8.h \
+ C:\Users\crist\Documents\work\summer-school\summerschool\Lab\day1\LPC55S69_ex5\device/system_LPC55S69_cm33_core0.h \
+ C:\Users\crist\Documents\work\summer-school\summerschool\Lab\day1\LPC55S69_ex5\device/LPC55S69_cm33_core0_features.h \
+ C:\Users\crist\Documents\work\summer-school\summerschool\Lab\day1\LPC55S69_ex5\drivers/fsl_common_arm.h \
+ C:\Users\crist\Documents\work\summer-school\summerschool\Lab\day1\LPC55S69_ex5\drivers/fsl_clock.h \
+ C:\Users\crist\Documents\work\summer-school\summerschool\Lab\day1\LPC55S69_ex5\drivers/fsl_common.h \
+ C:\Users\crist\Documents\work\summer-school\summerschool\Lab\day1\LPC55S69_ex5\drivers/fsl_reset.h \
+ C:\Users\crist\Documents\work\summer-school\summerschool\Lab\day1\LPC55S69_ex5\component\timer_manager/fsl_component_timer_manager.h \
+ ../component/button/fsl_component_button.h
+C:\Users\crist\Documents\work\summer-school\summerschool\Lab\day1\LPC55S69_ex5\component\gpio/fsl_adapter_gpio.h:
+C:\Users\crist\Documents\work\summer-school\summerschool\Lab\day1\LPC55S69_ex5\drivers/fsl_common.h:
+C:\Users\crist\Documents\work\summer-school\summerschool\Lab\day1\LPC55S69_ex5\device/fsl_device_registers.h:
+C:\Users\crist\Documents\work\summer-school\summerschool\Lab\day1\LPC55S69_ex5\device/LPC55S69_cm33_core0.h:
+C:\Users\crist\Documents\work\summer-school\summerschool\Lab\day1\LPC55S69_ex5\CMSIS/core_cm33.h:
+C:\Users\crist\Documents\work\summer-school\summerschool\Lab\day1\LPC55S69_ex5\CMSIS/cmsis_version.h:
+C:\Users\crist\Documents\work\summer-school\summerschool\Lab\day1\LPC55S69_ex5\CMSIS/cmsis_compiler.h:
+C:\Users\crist\Documents\work\summer-school\summerschool\Lab\day1\LPC55S69_ex5\CMSIS/cmsis_gcc.h:
+C:\Users\crist\Documents\work\summer-school\summerschool\Lab\day1\LPC55S69_ex5\CMSIS/mpu_armv8.h:
+C:\Users\crist\Documents\work\summer-school\summerschool\Lab\day1\LPC55S69_ex5\device/system_LPC55S69_cm33_core0.h:
+C:\Users\crist\Documents\work\summer-school\summerschool\Lab\day1\LPC55S69_ex5\device/LPC55S69_cm33_core0_features.h:
+C:\Users\crist\Documents\work\summer-school\summerschool\Lab\day1\LPC55S69_ex5\drivers/fsl_common_arm.h:
+C:\Users\crist\Documents\work\summer-school\summerschool\Lab\day1\LPC55S69_ex5\drivers/fsl_clock.h:
+C:\Users\crist\Documents\work\summer-school\summerschool\Lab\day1\LPC55S69_ex5\drivers/fsl_common.h:
+C:\Users\crist\Documents\work\summer-school\summerschool\Lab\day1\LPC55S69_ex5\drivers/fsl_reset.h:
+C:\Users\crist\Documents\work\summer-school\summerschool\Lab\day1\LPC55S69_ex5\component\timer_manager/fsl_component_timer_manager.h:
+../component/button/fsl_component_button.h:
---
+++ Lab/day1/Day1_ex5/Debug/source/subdir.mk
@@ -0,0 +1,34 @@
+################################################################################
+# Automatically-generated file. Do not edit!
+################################################################################
+
+# Add inputs and outputs from these tool invocations to the build variables 
+C_SRCS += \
+../source/Day1_ex5.c \
+../source/semihost_hardfault.c 
+
+C_DEPS += \
+./source/Day1_ex5.d \
+./source/semihost_hardfault.d 
+
+OBJS += \
+./source/Day1_ex5.o \
+./source/semihost_hardfault.o 
+
+
+# Each subdirectory must supply rules for building sources it contributes
+source/%.o: ../source/%.c source/subdir.mk
+	@echo 'Building file: $<'
+	@echo 'Invoking: MCU C Compiler'
+	arm-none-eabi-gcc -D__REDLIB__ -DCPU_LPC55S69JBD100 -DCPU_LPC55S69JBD100_cm33 -DCPU_LPC55S69JBD100_cm33_core0 -DSDK_OS_BAREMETAL -DSDK_DEBUGCONSOLE=1 -DCR_INTEGER_PRINTF -DPRINTF_FLOAT_ENABLE=0 -DSERIAL_PORT_TYPE_UART=1 -D__MCUXPRESSO -D__USE_CMSIS -DDEBUG -I"C:\Users\crist\Documents\work\summer-school\summerschool\Lab\day1\LPC55S69_ex5\board" -I"C:\Users\crist\Documents\work\summer-school\summerschool\Lab\day1\LPC55S69_ex5\source" -I"C:\Users\crist\Documents\work\summer-school\summerschool\Lab\day1\LPC55S69_ex5\utilities" -I"C:\Users\crist\Documents\work\summer-school\summerschool\Lab\day1\LPC55S69_ex5\drivers" -I"C:\Users\crist\Documents\work\summer-school\summerschool\Lab\day1\LPC55S69_ex5\component\uart" -I"C:\Users\crist\Documents\work\summer-school\summerschool\Lab\day1\LPC55S69_ex5\component\serial_manager" -I"C:\Users\crist\Documents\work\summer-school\summerschool\Lab\day1\LPC55S69_ex5\device" -I"C:\Users\crist\Documents\work\summer-school\summerschool\Lab\day1\LPC55S69_ex5\CMSIS" -I"C:\Users\crist\Documents\work\summer-school\summerschool\Lab\day1\LPC55S69_ex5\component\lists" -I"C:\Users\crist\Documents\work\summer-school\summerschool\Lab\day1\LPC55S69_ex5\startup" -I"C:\Users\crist\Documents\work\summer-school\summerschool\Lab\day1\LPC55S69_ex5\component\button" -I"C:\Users\crist\Documents\work\summer-school\summerschool\Lab\day1\LPC55S69_ex5\component\timer_manager" -I"C:\Users\crist\Documents\work\summer-school\summerschool\Lab\day1\LPC55S69_ex5\component\gpio" -I"C:\Users\crist\Documents\work\summer-school\summerschool\Lab\day1\LPC55S69_ex5\component\timer" -O0 -fno-common -g3 -gdwarf-4 -Wall -c -ffunction-sections -fdata-sections -ffreestanding -fno-builtin -fmerge-constants -fmacro-prefix-map="$(<D)/"= -mcpu=cortex-m33 -mfpu=fpv5-sp-d16 -mfloat-abi=hard -mthumb -D__REDLIB__ -fstack-usage -specs=redlib.specs -MMD -MP -MF"$(@:%.o=%.d)" -MT"$(@:%.o=%.o)" -MT"$(@:%.o=%.d)" -o "$@" "$<"
+	@echo 'Finished building: $<'
+	@echo ' '
+
+
+clean: clean-source
+
+clean-source:
+	-$(RM) ./source/Day1_ex5.d ./source/Day1_ex5.o ./source/semihost_hardfault.d ./source/semihost_hardfault.o
+
+.PHONY: clean-source
+
