c460fb16fe640cec9f2f160be48fea3deabb89b8
Malutan <malutan_sergiu@yahoo.com>
2024-06-27 21:34:33 +0300

    Add files via upload

---
+++ Lab/day4/lab4_solved/source/AES/AES.h
@@ -0,0 +1,48 @@
+#ifndef _AES_H_
+#define _AES_H_
+
+#include <stdint.h>
+#include <stddef.h>
+
+// #define the macros below to 1/0 to enable/disable the mode of operation.
+//
+// CBC enables AES encryption in CBC-mode of operation.
+
+
+//#define AES128 1
+//#define AES192 1
+#define AES256 1
+
+#define AES_BLOCKLEN 16 // Block length in bytes - AES is 128b block only
+
+#if defined(AES256) && (AES256 == 1)
+    #define AES_KEYLEN 32
+    #define AES_keyExpSize 240
+#elif defined(AES192) && (AES192 == 1)
+    #define AES_KEYLEN 24
+    #define AES_keyExpSize 208
+#else
+    #define AES_KEYLEN 16   // Key length in bytes
+    #define AES_keyExpSize 176
+#endif
+
+struct AES_ctx
+{
+  uint8_t RoundKey[AES_keyExpSize];
+  uint8_t Iv[AES_BLOCKLEN];
+};
+
+void AES_init_ctx(struct AES_ctx* ctx, const uint8_t* key);
+
+// buffer size is exactly AES_BLOCKLEN bytes;
+
+void AES_ECB_encrypt(const struct AES_ctx* ctx, uint8_t* buf);
+void AES_ECB_decrypt(const struct AES_ctx* ctx, uint8_t* buf);
+
+void AES_CBC_encrypt(const struct AES_ctx* ctx, uint8_t* buf, uint8_t blockNum);
+void AES_CBC_decrypt(const struct AES_ctx* ctx, uint8_t* buf, uint8_t blockNum);
+
+void AES_CTR_xcrypt_buffer(struct AES_ctx* ctx, uint8_t* buf, size_t length);
+
+void XOR(uint8_t* buf1, uint8_t* buf2);
+#endif // _AES_H_
